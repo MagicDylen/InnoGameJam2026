1e32f1026bd1de180477fc29f2a550d3b1aa7fcf
Natasha Libera <tash@artoftash.com>
2026-01-31 00:16:16 +0100

    Created MainMenuScene with placeholder visuals

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MaskPileGame
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            Debug.Log("Player wanna play!");
+            // SceneManager.LoadScene("StartingScene");
+        }
+        public void OpenSettings()
+        {
+            Debug.Log("Player want some advanced QOL features."); 
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Player super bored now. Get me out of here!");
+            Application.Quit(); 
+        }
+    }
+}
