c23de04f8552b3d92249e5e5ac69d39a22c5ca76
Nik <nhillebrecht@gmail.com>
2026-02-01 12:53:29 +0100

    FEATURE: health bar indicator

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    UnityEngine.UI.RawImage image;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image = GetComponent<UnityEngine.UI.RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player stats and current max health divided by total health
+        PlayerStats playerStats = FindFirstObjectByType<PlayerStats>();
+        float healthPercent = (float)playerStats.TotalHealth / (float)playerStats.MaxHealth;
+
+        transform.localScale = new Vector3(healthPercent, 1, 1);
+        image.uvRect = new Rect(0, 0, healthPercent, 1);
+        transform.localPosition = new Vector3(-((1 - healthPercent) * 0.5f * 170) - 18, transform.localPosition.y, transform.localPosition.z);
+    }
+}
